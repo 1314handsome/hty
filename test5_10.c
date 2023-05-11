e5989f20becca175c44a05579ebad3a9723a144a
1314handsome <2540578214@qq.com>
2023-05-11 21:51:01 +0800

    学习代码

---
+++ test5_10.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+//int main()
+//{
+//	int a=1000;
+//	FILE* pf=fopen("text.txl","a");
+//	//fwrite(&a,4,1,pf);
+//	fclose(pf);
+//	pf=NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pfwrite=fopen("test.txt","w");
+//	if(pfwrite==NULL)
+//	{
+//		printf("%s\n",strerror(errno));
+//	}
+//	fputc('b',pfwrite);
+//	fputc('i',pfwrite);
+//	fputc('t',pfwrite);
+//	fclose(pfwrite);
+//	pfwrite=NULL;
+//	return 0;
+//}
+
+//int main()
+//{
+//	FILE* pfride=fopen("test.txt","r");
+//	if(pfride==NULL)
+//	{
+//		printf("%s\n",strerror(errno));
+//	}
+//	printf("%c",fgetc(pfride));
+//	printf("%c",fgetc(pfride));
+//	printf("%c",fgetc(pfride));
+//	fclose(pfride);
+//	pfride=NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	fputc('h',stdout);
+//	printf("%c\n",fgetc(stdout));
+//	return 0;
+//}
+
